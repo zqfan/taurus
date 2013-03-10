bff6057e1defd083cfa793e907976e469ad71d41
Ajiva Fan <aji.zqfan@gmail.com>
2013-03-10 21:37:38 +0800

    crack-code-interview

---
+++ crack-the-coding-interview/1.1.c
@@ -0,0 +1,3 @@
+/* Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures? */
+
+/* i don't quite understand what *all unique characters* means */
