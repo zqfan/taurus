039dc5640712e2e56b49dc9bbef1181b4a4417cb
Ajiva Fan <aji.zqfan@gmail.com>
2013-03-21 12:29:34 +0800

    add cnweaher

---
+++ cnweather.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 softtabstop=4 shiftwidth=4
+
+import sys
+import json
+import re
+
+import argparse
+from urllib import quote
+import requests
+
+
+def get_argparse():
+    args = argparse.ArgumentParser(
+        description='get weather from www.weather.com.cn')
+    args.add_argument('-c','--city-code',
+        default='101010100', # beijing
+        dest='city_code',
+        help='citycode of your city')
+    args.add_argument("-n","--city-name",
+        default="beijing",
+        dest="city_name",
+        help="city name of your city")
+    return args
+
+
+def print_weather_data(city_code):
+    data_url = 'http://m.weather.com.cn/data/%s.html' % city_code
+
+    try:
+        resp = requests.get(data_url)
+    except:
+        return
+    if resp.status_code != 200:
+        return
+    content = resp.content.replace("℃","'C")
+    content = content.replace('~',' / ')
+    data = json.loads(content)['weatherinfo']
+    print data["city"]
+    print data['temp1'], data['weather1'], data['wind1']
+    print data['temp2'], data['weather2'], data['wind2']
+    
+
+def get_city_code(city_name):
+    city_name = quote(city_name) # url encode
+    url = ("http://toy.weather.com.cn/SearchBox/searchBox?"
+        "callback=jsonp1342857491709&_=1342857620727"
+        "&language=zh&keyword=%s" % city_name)
+    resp = requests.get(url)
+    content = resp.content.split("(")[1].split(")")[0]
+    city_code = json.loads(content)["i"][0]["i"]
+    return city_code
+
+
+def main():
+    args = get_argparse()
+    args.parse_args(sys.argv[1:], args)
+    if args.city_name:
+        args.city_code = get_city_code(args.city_name)
+    print_weather_data(args.city_code)
+    
+
+if __name__ == '__main__':
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    main()
--- job-digger/job-digger.py
+++ job-digger/job-digger.py
@@ -7,6 +7,7 @@
 
 import json
 import time
+import logging
 
 import driver
 
@@ -26,9 +27,17 @@ class JobDigger(object):
         @companies: tulpe of companies, which indicate series DiggerDriver
                     according to the name
         """
+        self._init_log()
         self._init_driver(companies)
         self.load_env()
         self.load_job_info()
+
+    def _init_log(self):
+        format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        logging.basicConfig(filename="log",format=format)
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._logger.setLevel(logging.DEBUG)
+        self._logger.info("initialization finished")
 
     def _init_driver(self, companies):
         if not companies:
@@ -55,6 +64,7 @@ class JobDigger(object):
                 if info["link"] in links:
                     continue
                 self.job_info.append(info)
+        self._logger.info("get job info finished.")
 
     def dump_job_info(self):
         """Dump job info to a local file in json format."""
@@ -64,19 +74,22 @@ class JobDigger(object):
         date = time.strftime("%Y-%m-%d", time.localtime())
         self.update_env({"last_update":date})
         self.dump_env()
+        self._logger.info("dump job info finished. total %d.",
+            len(self.job_info))
 
     def load_job_info(self):
         """Load job info from a local file in json format."""
         fp = open("data", "r")
         self.job_info = json.load(fp)
         fp.close()
+        self._logger.info("load job info from local file finished.")
 
     def load_env(self):
         """Load environment from a local file in json format.
 
         Currently, only 'last_update' opt is used.
         """
-        fp = open(".env", "r")
+        fp = open("env", "r")
         self.env = json.load(fp)
         fp.close()
 
@@ -84,12 +97,14 @@ class JobDigger(object):
         """Update env from a dict."""
         for key in kwargs:
             self.env[key] = kwargs[key]
+        self._logger.info("env has been updated.")
 
     def dump_env(self):
         """Dumpp environment to a local file in json format."""
-        fp = open(".env", "w")
+        fp = open("env", "w")
         json.dump(self.env, fp, indent=2)
         fp.close()
+        self._logger.info("env has been dumped.")
 
     def print_job_info(self):
         """print all job info."""
