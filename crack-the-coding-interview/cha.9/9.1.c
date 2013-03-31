4f48c098cf82b8f825e6e711fda51edb7c725b93
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-03-31 19:34:23 +0800

    cha.9.5 done

--- crack-the-coding-interview/cha.9/9.1.c
+++ crack-the-coding-interview/cha.9/9.1.c
@@ -1,9 +1,15 @@
+/* 9.1
+   You are given two sorted arrays, A and B, and A has a large enough
+   buffer at the end to hold B. Write a method to merge B into A in
+   sorted order.
+*/
 #include <stdio.h>
 
 /* 1. len_t is the element count of target's, so does len_s to source.
    2. the buffer of target should can hold all elements of target and
    source.
-   3. target and source has already been sorted in asce order. */
+   3. target and source has already been sorted in asce order.
+*/
 void merge_by_sort(int * target, int len_t,
                    const int * source, int len_s)
 {
