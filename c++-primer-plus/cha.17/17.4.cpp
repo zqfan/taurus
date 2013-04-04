8d94be086e7b41f4cd48fb8d4492fc28a6296215
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-04-04 16:59:37 +0800

    c++ prime plus cha.17.4 done

--- c++-primer-plus/cha.17/17.4.cpp
+++ c++-primer-plus/cha.17/17.4.cpp
@@ -15,3 +15,47 @@
    eggs kites donuts zero lassitude
    balloons hammers finance drama
    stones */
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+  cout << "Concatenate two files to a new file.\n"
+       << "Usage: " << argv[0] << " source_file_1 source_file_2 "
+       << "target_file" << endl;
+  if ( argc < 4 ) {
+    cerr << "ERROR: need 3 argument but only " << argc - 1
+         << " given" << endl;
+    return -1;
+  }
+  ifstream fin1(argv[1]);
+  if ( ! fin1.is_open() ) {
+    cerr << "ERROR: " << argv[1] << " file not found." << endl;
+    return -1;
+  }
+  ifstream fin2(argv[2]);
+  if ( ! fin2.is_open() ) {
+    cerr << "ERROR: " << argv[1] << " file not found." << endl;
+    fin1.close();
+    return -1;
+  }
+  ofstream fout(argv[3]);
+  string line1, line2, target;
+  while ( getline(fin1, line1) ) {
+    if ( getline(fin2, line2) ) {
+      target = line1 + " " + line2;
+      fout << target << endl;
+    }
+    else
+      fout << line1 << endl;
+  }
+  while ( getline(fin2, line2) )
+    fout << line2 << endl;
+  fin1.close();
+  fin2.close();
+  fout.close();
+  cout << "Job done." << endl;
+  return 0;
+}
