1f9ed94e48fd16abe678cc67833fe872e32956ce
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-04-18 15:08:39 +0800

    Add a better way for fibonacci

--- crack-the-coding-interview/cha.08/8.1.py
+++ crack-the-coding-interview/cha.08/8.1.py
@@ -29,6 +29,13 @@ def fibonacci2(n, result=None):
         result[n-2] = fibonacci2(n-2, result)
     return result[n-1] + result[n-2]
 
+
+def fibonacci3(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a+b
+    return a
+
 if __name__ == "__main__":
     import sys
-    print fibonacci2(int(sys.argv[1]))
+    print fibonacci3(int(sys.argv[1]))
