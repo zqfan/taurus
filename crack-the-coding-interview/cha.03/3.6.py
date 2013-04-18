c5e5dcf57a908eedea6bb663bd4453a39f1fd0ca
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-04-18 15:40:12 +0800

    crack the coding interview cha03.06 done

--- crack-the-coding-interview/cha.03/3.6.py
+++ crack-the-coding-interview/cha.03/3.6.py
@@ -7,6 +7,26 @@ tions about how the stack is implemented. The following are the only functions t
 should be used to write this program: push | pop | peek | isEmpty.
 """
 
+import stack
 
-def sort_stack(stack):
-    pass
+def sort_stack(s):
+    tmp_stack = stack.Stack()
+    while not s.isEmpty():
+        top_element = s.pop()
+        while (not tmp_stack.isEmpty()) and tmp_stack.peek() > top_element:
+            s.push(tmp_stack.pop())
+        tmp_stack.push(top_element)
+    return tmp_stack
+
+
+def main():
+    s = stack.Stack()
+    s.push(2)
+    s.push(1)
+    s.push(3)
+    s = sort_stack(s)
+    print s.pop(), s.pop(), s.pop()
+
+
+if __name__ == '__main__':
+    main()
